2e87464b0a59bd810ae10e6348cffaa44e4b80ab
shalinishah27 <shalinishah27.temp@gmail.com>
2022-11-27 16:16:12 -0500

    Search word validation and Word suggestion

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -9,6 +9,7 @@ import model.CreditCard;
 import services.WebDriverService;
 import services.CreditCardFetcher;
 import services.KeyWordService;
+import services.SearchWord;
 import services.WebCrawler;
 import services.CreateTextFiles;
 
@@ -53,20 +54,40 @@ public class MainApplication {
 			sb.append("\n\nRecently searched keyword: " + KeyWordService.getLastSearchedKeywordWithFrequency());
 			System.out.println(sb.toString());
 			
-			
-			// temp test code for reading input
-			System.out.println("Please enter your keywords (comma separated) and hit the return key to proceed ...");
-			
-			String inputLine = scanner.nextLine();
-			String[] inputKeyWords = inputLine.split(",");
-			KeyWordService.saveKeywords(Util.listify(inputKeyWords));
-			
-			// Simulating the pending work below
-//			System.out.println("Based on your search, we recommend you " + creditCards.get(0).getName());
+			//validation and search 
+			boolean isInputValid = false;
+			String[] inputKeyWords;
+			do {
+				System.out.println("Please enter your keywords (comma separated) and hit the return key to proceed ...");
+				
+				String inputLine = scanner.nextLine();
+				inputKeyWords = inputLine.split(",");
+				if(!validateInput(inputKeyWords)) {
+					System.out.println("Search keyword cannot have numbers in it.");
+					isInputValid = false;
+				}
+				else {
+					 isInputValid = true;
+					 KeyWordService.saveKeywords(Util.listify(inputKeyWords));
+				}
+			}while(!isInputValid);
 			
 			// riteesh code will execute to create text files from html files and store them in resources -> TextFiles
 			CreateTextFiles.createFiles(); // function to create text files
 			CreateTextFiles.createInvertedIndexMap(); // function to create inverted frequency map
+			
+			for (int i = 0; i < inputKeyWords.length; i++) {
+				SearchWord searchword = new SearchWord();
+				searchword.suggestAltWord(inputKeyWords[i].trim());
+				if(searchword.matchedWord != null && searchword.matchedWord.length() > 0) {
+					System.out.println("Press Y:");
+					String ans = scanner.nextLine();
+					
+					if(ans.equalsIgnoreCase("Y")) {
+						inputKeyWords[i] = searchword.matchedWord;
+					}
+				}
+			}
 			
 			System.out.println("Do you want to start over? (Please press y to continue)");
 			String response = scanner.nextLine();
@@ -77,4 +98,14 @@ public class MainApplication {
 		System.out.println("Thank you for your interest. Have a nice day!");
 	}
 	
+	public static boolean validateInput(String[] str) 
+	{
+		for (int i = 0; i < str.length; i++) {
+			String string = str[i];
+			if(!(string!= null && !string.equalsIgnoreCase("") && (string.matches("^[a-zA-Z]*$") || string.matches("^[a-zA-Z_ ]*$"))))
+				return false;	
+				
+		}
+		return true; 
+	}
 }
